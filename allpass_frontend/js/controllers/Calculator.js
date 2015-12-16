99d0a9d022893f6aad2b92e38e0be6302a3d3ed7
Jacob Aloysious <jacobaloysious@gmail.com>
2015-12-16 09:43:51 +0530

    Added test framework support JASMINE AND KARMA dependencies. Initial commit

---
+++ allpass_frontend/js/controllers/Calculator.js
@@ -0,0 +1,11 @@
+﻿var Calculator = function () {
+
+};
+
+Calculator.prototype.add = function (a, b) {
+    return a + b;
+};
+
+Calculator.prototype.divide = function (a, b) {
+    return a / b;
+};
---
+++ allpass_frontend/test/spec/CourseControllerSpec.js
@@ -0,0 +1,24 @@
+﻿describe("Calculator", function () {
+
+    var calc;
+    beforeEach(function () {
+        calc = new Calculator();
+
+
+    });
+
+    it("testing addition", function () {
+        var val = calc.add(1, 2);
+        expect(calc.add(1, 1)).toBe(2);
+    });
+
+    it("testing divide by 2", function () {
+        expect(calc.divide(4, 2)).toBe(2);
+    });
+
+    xit("testing divide 1 by 2", function () {
+        expect(calc.divide(1, 3)).toBeBetween(0.3, 0.4);
+    });
+
+
+});
